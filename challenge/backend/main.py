0bfa07eea39e374c2e2c443b8e6845a5011ba0fa
Siboniso Ndlovu <sibonisow.ndlovu@gmail.com>
2023-08-27 15:16:09 +0200

    Added loggin and timing

--- challenge/backend/main.py
+++ challenge/backend/main.py
@@ -6,19 +6,28 @@ import logging
 import time
 from shapely.geometry import Point
 
+
+def time_convert(sec):
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+    return "{0}:{1}:{2}".format(int(hours), int(mins), sec)
+
+
 start_time = time.time()
 
 # Set the error threshold for logging
 error_threshold = 0.1  # 10%
 
+root_dir = ""
 # Load the service request dataset
 service_requests = pd.read_csv(
-    'challenge/backend/sr.csv')
-
+    './sr.csv')
 
 # Load the hexagon polygons from the GeoJSON file
 hex_polygons = gpd.read_file(
-    'challenge/backend/city-hex-polygons-8.geojson')
+    './city-hex-polygons-8.geojson')
 
 svc_req_geo = [Point(xy) for xy in zip(
     service_requests['longitude'], service_requests['latitude'])]
@@ -32,12 +41,13 @@ svc_regdf = gpd.GeoDataFrame(
 joined = gpd.sjoin(svc_regdf, hex_polygons, how="left", op='within')
 
 validation_data = pd.read_csv(
-    'challenge/backend/sr_hex.csv')
+    './sr_hex.csv')
 
-validation_data['in_geo_df'] = validation_data.apply(lambda row: (
-    row['notification_number'], row['h3_level8_index']) in zip(joined['notification_number'], joined['index']), axis=1)
+failed_to_join = 0
+for id in zip(joined['notification_number'], joined['index']):
+    if (pd.isna(id[1])):
+        failed_to_join += 1
 
-# Print rows that are not present in the GeoDataFrame
-rows_not_in_joined = validation_data[~validation_data['in_geo_df']]
-print("Rows not present in GeoDataFrame:")
-print(rows_not_in_joined)
+end_time = time.time()
+print(f"Time taken: {time_convert(end_time - start_time)}")
+print(f"Failed join: {failed_to_join}")
