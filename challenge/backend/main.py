280df1ad8ddadf6836a99e728cfd1526fc07cbfb
Siboniso Ndlovu <43060555+SibonisoNdlovu@users.noreply.github.com>
2023-08-27 21:02:39 +0200

    Update main.py

--- challenge/backend/main.py
+++ challenge/backend/main.py
@@ -5,6 +5,7 @@ import h3
 import logging
 import time
 from shapely.geometry import Point
+import zipfile
 
 
 def time_convert(sec):
@@ -16,18 +17,20 @@ def time_convert(sec):
 
 
 start_time = time.time()
+print(f"Process Started")
 
 # Set the error threshold for logging
 error_threshold = 0.1  # 10%
 
 root_dir = ""
-# Load the service request dataset
-service_requests = pd.read_csv(
-    './sr.csv')
+
+service_requests = pd.read_csv('https://cct-ds-code-challenge-input-data.s3.af-south-1.amazonaws.com/sr.csv.gz', compression='gzip',)
+print(f"Loaded Service Requests")
 
 # Load the hexagon polygons from the GeoJSON file
 hex_polygons = gpd.read_file(
-    './city-hex-polygons-8.geojson')
+    'https://cct-ds-code-challenge-input-data.s3.af-south-1.amazonaws.com/city-hex-polygons-8.geojson')
+print(f"Loaded Hex Polygons from GeoJson File")
 
 svc_req_geo = [Point(xy) for xy in zip(
     service_requests['longitude'], service_requests['latitude'])]
@@ -39,9 +42,10 @@ svc_regdf = gpd.GeoDataFrame(
     service_requests, crs="EPSG:4326", geometry=svc_req_geo)
 
 joined = gpd.sjoin(svc_regdf, hex_polygons, how="left", op='within')
+print(f"Data joined")
 
-validation_data = pd.read_csv(
-    './sr_hex.csv')
+validation_data = pd.read_csv('https://cct-ds-code-challenge-input-data.s3.af-south-1.amazonaws.com/sr_hex.csv.gz', compression='gzip',)
+print(f"Data validated")
 
 failed_to_join = 0
 for id in zip(joined['notification_number'], joined['index']):
